73d46c9e198dbe59c7e32a23f779411605444ac3
gafiirfandi <gafiirfandi45@gmail.com>
2020-12-17 22:47:57 +0700

    fixing react-bootstrap

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Col, Container, Row } from "react-bootstrap";
 
 class App extends Component {
   constructor(props) {
@@ -8,7 +9,20 @@ class App extends Component {
   }
 
   render() {
-    return <h1>Haludulu Django X React test huhu</h1>;
+    return (
+      <Container>
+        <Row>
+          <Col>1 of 3</Col>
+          <Col xs={6}>2 of 3 (wider)</Col>
+          <Col>3 of 3</Col>
+        </Row>
+        <Row>
+          <Col>1 of 3</Col>
+          <Col xs={5}>2 of 3 (wider)</Col>
+          <Col>3 of 3</Col>
+        </Row>
+      </Container>
+    );
   }
 }
 

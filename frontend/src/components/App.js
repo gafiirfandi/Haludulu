7b878cb67ee8bd4722631ea611e37bb2d7d3bc65
DestaraSyanandi <destasyanando@gmail.com>
2020-12-17 21:45:56 +0700

    pull install bootstrap

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,13 +1,11 @@
-import React, { Component } from "react";
+import React from "react";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return <h1>Haludulu Django X React test huhu</h1>;
-  }
+function App() {
+  return (
+    <div>
+      <h1>halo</h1>
+    </div>
+  );
 }
 
 export default App;

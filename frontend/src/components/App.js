cccae9adef6d5bb15b6489b30987580b2404444b
gafiirfandi <gafiirfandi45@gmail.com>
2020-12-17 21:14:50 +0700

    Recommiting without env and node_modules folder

---
+++ frontend/src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <h1>Haludulu Django X React test huhu</h1>;
+  }
+}
+
+export default App;

76628e899b48cd1780296f28e859edf7d3583495
gafiirfandi <gafiirfandi45@gmail.com>
2020-12-27 13:39:14 +0700

    Fixed navbar searchbar and logo, also fixed detail page routing

--- frontend/src/components/pages/Detail.js
+++ frontend/src/components/pages/Detail.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Detailpage from "../Detailpage";
 
 function Detail(props) {
+  useEffect(() => {
+    console.log(
+      props.location.pathname[props.location.pathname.length - 1],
+      "pathname"
+    );
+  }, []);
   return (
     <div style={{ overflow: "hidden" }}>
-      <Detailpage id={props.location.state.id} {...props}/>
+      <Detailpage
+        id={props.location.pathname[props.location.pathname.length - 1]}
+        {...props}
+      />
     </div>
   );
 }

ce5b0797669bafee398a8f611a6162219dbf240b
DestaraSyanandi <destasyanando@gmail.com>
2020-12-31 19:07:48 +0700

    pull all updates

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -11,49 +11,12 @@ function Homepage({ products, admin, setShowSearch, setSearchKeyword }) {
   useEffect(() => {
     Aos.init({ duration: 2000 });
   }, []);
-  // const [width, setWidth] = useState(0)
-
-  // useEffect(() => {
-  //   let bajuBox = document.getElementsByClassName("BajuBox");
-  //   // width = bajuBox[0].clientWidth
-  //   setWidth(bajuBox[0].clientWidth)
-  //   // let bajuBox = document.getElementsByClassName("BajuBox");
-  //   // let width = bajuBox[0].clientWidth
-  //   // console.log(width)
-  //   for(let i = 0; i < bajuBox.length; i++){
-  //     bajuBox[i].style.height = width + "px"
-  //     console.log(bajuBox[i].clientHeight)
-
-  //   }
-  // })
-
-  // let bajuBox = document.getElementsByClassName("BajuBox");
-  // let width = bajuBox[0].width
-  // bajuBox.forEach(element => {
-  //   element.height = width + 'px'
-  // });
-  // $('.child').css({'height':cw+'px'});
-
-  // const [products, setProducts] = useState([]);
-
-  // useEffect(() => {
-  //   async function fetchData() {
-  //     console.log(axios.defaults.baseURL + "/api");
-  //     const request = await axios.get(axios.defaults.baseURL + "/api");
-  //     console.log(request.data);
-  //     setProducts(request.data);
-  //     // setMovies(request.data.results);
-  //     return request;
-  //   }
-  //   fetchData();
-  // }, []);
 
   const getPrice = (price) => {
-    // console.log(price, "- price");
     let str_price = price.toString().split("").reverse().join("");
     let str_price_idr = "";
     let counter = 1;
-    // console.log(str_price, "- str price");
+
     let len = str_price.length;
     for (let char in str_price) {
       str_price_idr += str_price[char];
@@ -63,7 +26,7 @@ function Homepage({ products, admin, setShowSearch, setSearchKeyword }) {
       }
       counter++;
     }
-    // console.log(str_price_idr.split().reverse().join(""));
+
     return str_price_idr.split("").reverse().join("");
   };
 
@@ -82,21 +45,16 @@ function Homepage({ products, admin, setShowSearch, setSearchKeyword }) {
                 to={{
                   pathname: "/detail/" + product.id,
                   state: { id: product.id },
-                }}>
-                {/* <Link
-                to={{
-                  pathname: "/detail/" + product.id,
-                  // search: "?sort=name",
-                  // hash: "#the-hash",
-                  state: { fromDashboard: true },
-                }}> */}
+                }}
+              >
                 <div
                   className="BajuBox"
                   onClick={() => {
                     console.log("yey");
                     setSearchKeyword("");
                     setShowSearch(false);
-                  }}>
+                  }}
+                >
                   <img src={product.main_img} className="ImgBaju" alt="helo" />
                   {admin && (
                     <div className="ButtonAdmin">
@@ -106,7 +64,8 @@ function Homepage({ products, admin, setShowSearch, setSearchKeyword }) {
                           to={{
                             pathname: "/update_item/" + product.id,
                             state: { id: product.id },
-                          }}>
+                          }}
+                        >
                           Update
                         </Link>
                       </button>
@@ -114,7 +73,8 @@ function Homepage({ products, admin, setShowSearch, setSearchKeyword }) {
                       <button
                         onClick={() => handleDelete(product.id)}
                         type="button"
-                        className="ButtonDelete">
+                        className="ButtonDelete"
+                      >
                         Delete
                       </button>
                     </div>
@@ -132,46 +92,6 @@ function Homepage({ products, admin, setShowSearch, setSearchKeyword }) {
             </Col>
           );
         })}
-        {/* <Col xs="6" sm="6" lg className="ColumnBaju">
-          <div className="BajuBox">Halo</div>
-        </Col>
-        <Col xs="6" sm="6" lg className="ColumnBaju">
-          <div className="BajuBox">Halo</div>
-        </Col>
-        <Col xs="6" sm="6" lg className="ColumnBaju">
-          <div className="BajuBox">Halo</div>
-        </Col>
-        <Col xs="6" sm="6" lg className="ColumnBaju">
-          <div className="BajuBox">Halo</div>
-        </Col>
-      </Row>
-      <Row>
-        <Col xs="6" sm="6" lg className="ColumnBaju">
-          <div className="BajuBox">Halo</div>
-        </Col>
-        <Col xs="6" sm="6" lg className="ColumnBaju">
-          <div className="BajuBox">Halo</div>
-        </Col>
-        <Col xs="6" sm="6" lg className="ColumnBaju">
-          <div className="BajuBox">Halo</div>
-        </Col>
-        <Col xs="6" sm="6" lg className="ColumnBaju">
-          <div className="BajuBox">Halo</div>
-        </Col>
-      </Row>
-      <Row>
-        <Col xs="6" sm="6" lg className="ColumnBaju">
-          <div className="BajuBox">Halo</div>
-        </Col>
-        <Col xs="6" sm="6" lg className="ColumnBaju">
-          <div className="BajuBox">Halo</div>
-        </Col>
-        <Col xs="6" sm="6" lg className="ColumnBaju">
-          <div className="BajuBox">Halo</div>
-        </Col>
-        <Col xs="6" sm="6" lg className="ColumnBaju">
-          <div className="BajuBox">Halo</div>
-        </Col> */}
       </Row>
     </div>
   );
